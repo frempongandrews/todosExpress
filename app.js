28b22239f888ba029785f90f3b775d1c9e3d14f6
Andrews Frempong <frempongandrews@yahoo.com>
2016-12-29 00:20:33 +0000

    test todo array on [/todos]

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var PORT = process.env.PORT || 4000;
 var app = express();
-var PORT = 4000;
-app.get('/api', function (req, res) {
-        res.send("express working");
+
+app.use(express.static("public"));
+app.set("views", "./public");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/public/index.html");
+});
+
+app.get("/todos", function (req, res) {
+   res.json({
+       "todos" : ["grocery", "primark", "milk"]
+   });
 });
 
 app.listen(PORT, function () {
